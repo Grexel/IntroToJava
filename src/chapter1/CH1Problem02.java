161008083b92313db5e714a20b0751ebbb84447d
Jeffrey Miller <millerjl07@mounties.mansfield.edu>
2017-05-21 14:24:15 -0400

    Problems 1-3 Chapter 1

---
+++ src/chapter1/CH1Problem02.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter1;
+
+/**
+ * Display five messages
+ * @author JeffreyMiller
+ */
+public class CH1Problem02 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++){
+            System.out.println("Welcome to Java");
+        }
+    }
+}
---
+++ src/chapter1/CH1Problem03.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter1;
+
+/**
+ * Display a pattern
+ * @author JeffreyMiller
+ */
+public class CH1Problem03 {
+    public static void main(String[] args) {
+        System.out.println("     J     A    V     V    A   ");
+        System.out.println("     J    A A    V   V    A A  ");
+        System.out.println(" J   J   AAAAA    V V    AAAAA ");
+        System.out.println("  J J   A     A    V    A     A");
+    }
+}
