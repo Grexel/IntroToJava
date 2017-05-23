a447cceafeddf89d3d2096675f0cdb0a0d9298bc
Jeffrey Miller <millerjl07@mounties.mansfield.edu>
2017-05-23 16:40:03 -0400

    Problems 4 Chapter 1

---
+++ src/chapter1/CH1Problem04.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter1;
+
+/**
+ *
+ * @author JeffreyMiller
+ */
+public class CH1Problem04 {
+    public static void main(String[] args) {
+        System.out.println("a\ta^2\ta^3");
+        for(int i = 1; i < 5; i++){
+            System.out.println(i + "\t" + (i*i) + "\t" + (i*i*i));
+        }
+    }
+}
