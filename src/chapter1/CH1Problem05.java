ac23a6b5417e8faf1cdfc1ed9bfd2f467f467be0
Jeffrey Miller <millerjl07@mounties.mansfield.edu>
2017-05-23 16:43:44 -0400

    Problem 5 Chapter 1

---
+++ src/chapter1/CH1Problem05.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter1;
+
+/**
+ * Compute an expression
+ * @author JeffreyMiller
+ */
+public class CH1Problem05 {
+    public static void main(String[] args) {
+        double numerator = 9.5 * 4.5 - 2.5 * 3;
+        double denominator = 45.5 - 3.5;
+        double answer = numerator / denominator;
+        System.out.println("The answer is: " + answer);
+    }
+}
